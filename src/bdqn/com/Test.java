2ef387f65553431289e202636c05fb708ae2967a
zuosheng <3268657155@qq.com>
2017-05-27 15:12:19 +0800

    Initial commit

---
+++ src/bdqn/com/Test.java
@@ -0,0 +1,10 @@
+package bdqn.com;
+
+/**
+ * Created by 故人诉璃歌 on 2017/5/27.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hellow Word");
+    }
+}
